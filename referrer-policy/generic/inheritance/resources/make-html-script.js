35e1448e395fce6a1b1ce390aa474d8a15a92a81
Antonio Sartori <antoniosartori@chromium.org>
2020-10-15 07:23:07 -0700

    Add WP test for referrer policy inheritance when navigating back
    When navigating back (through history navigation) to a srcdoc iframe,
    we should expect the iframe to apply the same referrer policy it had
    originally, even if in the mean time the referrer policy of the main
    frame changed.
    
    This CL adds a web platform test for this behaviour. Although the test
    is failing at the moment in Chrome, we plan to fix this with the
    Policy Container (https://crbug.com/1130587).
    
    Bug: 1130587
    Change-Id: I843808fddfc0c6fa5d65ce88599bce51471aa691

--- referrer-policy/generic/inheritance/resources/make-html-script.js
+++ referrer-policy/generic/inheritance/resources/make-html-script.js
@@ -1,10 +1,13 @@
-function createScriptString(origin) {
-  return `<script src = "${origin}/common/security-features/resources/common.sub.js"><\/script>
-          <script>
-            requestViaXhr("${origin}/common/security-features/subresource/xhr.py").then(msg => {
-              top.postMessage({referrer: msg.referrer}, "*")
-            }).catch(e => {
-              top.postMessage({referrer: "FAILURE"}, "*");
-            });
+function createScriptString(origin, referrer) {
+  return `<script>
+            fetch("${origin}/common/security-features/subresource/xhr.py",`
+      + (referrer ? `{referrer: "${referrer}"}` : "")
+      + `)
+              .then(r => r.json())
+              .then(j => {
+                top.postMessage({referrer: j.headers.referer}, "*")
+              }).catch(e => {
+                top.postMessage({referrer: "FAILURE"}, "*");
+              });
           <\/script>`;
 }
